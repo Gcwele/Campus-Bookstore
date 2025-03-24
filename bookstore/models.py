9a47ec391ba5b5a4b2b5d7c62291349a1a153c95
Siseko23 <vuyolwethumhlamvu8@gmail.com>
2025-03-24 11:45:29 +0200

    Update models.py

--- bookstore/models.py
+++ bookstore/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class Customer(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
@@ -12,6 +13,8 @@ class Customer(db.Model, UserMixin):
 
     cart_item = db.relationship('Cart', backref=db.backref('customer', lazy=True))
     orders = db.relationship('Order', backref=db.backref('customer', lazy=True))
+    wishlist_items = db.relationship('Wishlist', backref=db.backref('customer', lazy=True))
+
 
     @property
     def password(self):
@@ -27,6 +30,7 @@ class Customer(db.Model, UserMixin):
     def __str__(self):
         return f'<Customer {self.id}>'
 
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable=False)
@@ -36,12 +40,17 @@ class Product(db.Model):
     product_picture = db.Column(db.String(1000), nullable=False)
     flash_sale = db.Column(db.Boolean, default=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    department=db.Column(db.String(100),nullable=False)
 
     carts = db.relationship('Cart', backref=db.backref('product', lazy=True))
     orders = db.relationship('Order', backref=db.backref('product', lazy=True))
+    wishlist = db.relationship('Wishlist', backref=db.backref('product', lazy=True))
+
+
 
     def __str__(self):
         return f'<Product {self.product_name}>'
+
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,6 +61,7 @@ class Cart(db.Model):
 
     def __str__(self):
         return f'<Cart {self.id}>'
+
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -65,3 +75,44 @@ class Order(db.Model):
 
     def __str__(self):
         return f'<Order {self.id}>'
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)  # Rating from 1 to 5
+    review_text = db.Column(db.Text, nullable=True)  # Optional review text
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Relationships
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    # Backreferences
+    customer = db.relationship('Customer', backref=db.backref('reviews', lazy=True))
+    product = db.relationship('Product', backref=db.backref('reviews', lazy=True))
+
+    def __str__(self):
+        return f'<Review {self.id} - Rating: {self.rating}>'
+
+
+# Wishlist Model
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Wishlist Item {self.id}>'
+
+    # Book Suggestion Model
+
+
+class BookSuggestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(100), nullable=False)
+    customer_email = db.Column(db.String(100), nullable=False)
+    book_title = db.Column(db.String(200), nullable=False)
+
+    suggestion_date = db.Column(db.DateTime, default=datetime.utcnow)
+
