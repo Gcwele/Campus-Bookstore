842673aa810e2df759401ca677d37b1fa1cde81f
Siseko23 <vuyolwethumhlamvu8@gmail.com>
2025-03-24 11:40:54 +0200

    Update __init__.py

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -1,30 +1,47 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import stripe
+import os
+
 db = SQLAlchemy()
+DB_NAME = "database.sqlite3"
 
-DB_NAME = 'database.sqlite3'
-
-
-def create_database():
-    with db.engine.connect() as connection:
+def create_database(app):
+    with app.app_context():
         db.create_all()
-    print('Database Created')
-
+    print("Database Created")
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config.from_object("config.Config")
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
+    stripe.api_key = app.config["STRIPE_SECRET_KEY"]  # Initialize Stripe
 
+    from .models import Customer, Cart, Product, Order
 
-    from .models import Customer, Cart, Product, Order  # Import before use
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
-    @app.errorhandler(404)
-    def page_not_found(error):
+    @login_manager.user_loader
+    def load_user(id):
+        return Customer.query.get(int(id))
+
+    from .views import views
+    from .auth import auth
+    from .admin import admin
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(admin, url_prefix="/")
+
+    create_database(app)
+
+    return app
         return render_template('404.html'), 404  # Add status code
 
     login_manager = LoginManager()
