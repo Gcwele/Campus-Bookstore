363acc012f168e95076336dd59816c0e3c251592
Siseko23 <vuyolwethumhlamvu8@gmail.com>
2025-03-25 12:29:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = create_app()
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
